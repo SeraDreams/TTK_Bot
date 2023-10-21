0324fcac2f739cdc7dff7a20ddbf184218feb1fa
Sera <a-nes.rnd@mail.ru>
2023-10-21 17:58:38 +0300

    update ticket recognition

--- ticket.py
+++ ticket.py
@@ -1,33 +1,42 @@
 import os
 
-import pytesseract
-import cv2
+from doctr.io import DocumentFile
+from doctr.models import ocr_predictor
 import re
 
 
-def text_recognising(file_path, text_file_name):
-    result = pytesseract.image_to_string(file_path, nice=3, config='--oem 3 --psm 6', lang='eng+rus')
-    with open(f'check_result/{text_file_name}', 'w') as file:
-        for line in result:
-            file.write(f'{line}')
+class FindTicketData:
+    def __init__(self, filepath: str):
+        self.filepath = self.check_filepath(filepath)
+        self.model = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_vgg16_bn', pretrained=True)
+        self.doc = DocumentFile.from_images(self.filepath)
 
-    with open(f'check_result_clear/{text_file_name}', "w") as file:
-        pattern = r"(?:\d\s?){14}"
-        matches = re.findall(pattern, result)
-        num = re.sub('\s', '', matches[0]) if matches else None
-        file.write(f'Ticket №{num}')
-        print(f'Ticket №{num}')
+    def start(self) -> str:
+        try:
+            res = self.model(self.doc)
+            for page in res.pages:
+                for block in page.blocks:
+                    for line in block.lines:
+                        num_ticket = ''
+                        for word in line.words:
+                            num_ticket += str(word.value)
+                            if (len(str(word.value)) == 13 or len(str(word.value)) == 14) and str(word.value).isdigit():
+                                return str(word.value)
+                        num_ticket_clear = re.sub(r'[^\d\.]', '', num_ticket)
+                        if (len(num_ticket_clear) == 13 or len(num_ticket_clear) == 14) and num_ticket_clear.isdigit():
+                            return num_ticket_clear
+        except Exception as e:
+            raise e
 
-    return f"Result wrote into {text_file_name}\n{'+' * 40}"
-
-
-def file_loader():
-    for file_name in os.listdir('check_img'):
-        file_path = f'check_img/{file_name}'
-        img = cv2.imread(file_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        print(text_recognising(file_path=img, text_file_name=f'result_{file_name.split(".")[0]}'))
+    @staticmethod
+    def check_filepath(f):
+        if not isinstance(f, str):
+            raise TypeError("filepath is not str")
+        return f
 
 
 if __name__ == "__main__":
-    file_loader()
+    for filename in os.listdir('check_img'):
+        filepath = f'check_img/{filename}'
+        with open(f'check_result/{filename.split(".")[0]}.txt', 'w') as file:
+            file.write(FindTicketData(filepath=filepath).start())
