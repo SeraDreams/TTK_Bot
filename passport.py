bf9cf39f2d59229eeefcca776bb10780af4ac19a
Sera <a-nes.rnd@mail.ru>
2023-10-21 17:58:24 +0300

    update passport recognition

--- passport.py
+++ passport.py
@@ -1,6 +1,7 @@
 from doctr.io import DocumentFile
 from doctr.models import ocr_predictor
 import re
+import os
 
 
 class FindPassportData:
@@ -9,17 +10,21 @@ class FindPassportData:
         self.model = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_vgg16_bn', pretrained=True)
         self.doc = DocumentFile.from_images(self.filepath)
 
-    def start(self) -> dict:
+    def start(self) -> str:
+        res = self.model(self.doc)
+        print(self.filepath)
+        print(res)
+        print('\n\n\n', '+' * 100, '\n\n\n')
         try:
-            res = self.model(self.doc)
             down_line = re.sub(r"[\D]", "", res.pages[0].blocks[-1].lines[-1].words[0].value)
-            nomer = down_line[:3] + down_line[17]
+            number = down_line[:3] + down_line[17]
             seria = down_line[3:9]
-            data = {"nomer": nomer, "seria": seria}
+            data = str(number) + ' ' + str(seria)
 
             return data
         except Exception as e:
-            raise e
+            return str(e)
+
 
     @staticmethod
     def check_filepath(f):
@@ -29,5 +34,7 @@ class FindPassportData:
 
 
 if __name__ == "__main__":
-    filepath = 'passport_img/img.png'
-    print(FindPassportData(filepath=filepath).start())
+    for filename in os.listdir('passport_img'):
+        filepath = f'passport_img/{filename}'
+        with open(f'passport_result/{filename.split(".")[0]}.txt', 'w') as file:
+            file.write(FindPassportData(filepath=filepath).start())
